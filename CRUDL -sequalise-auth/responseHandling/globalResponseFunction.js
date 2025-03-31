100e9040f8030d1eae30de17801508f30a2ea8a3
amit <amit.j@qrioustech.in>
2025-03-31 10:34:40 +0530

    crud project to different dir and new nasa project
    nasa project started

--- CRUDL -sequalise-auth/responseHandling/globalResponseFunction.js
+++
@@ -1,5 +0,0 @@
-function giveResponse(status, res, msg = { msg: "No Message handling" }) {
-  return res.status(status).json(msg);
-}
-
-module.exports = giveResponse;
--- timer/timer.js
+++
@@ -1,7 +0,0 @@
-const timer = document.getElementById("timer");
-console.log(timer);
-// setInterval(() => {
-//   //   const min = new Date().getMinutes();
-//   //   const sec = new Date().getSeconds();
-//   console.log(`${min} : ${sec}`);
-// }, 1000);
